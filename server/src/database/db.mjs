9f5756cab60a6ff5b3906a26534a71e7f38ff348
mikahimself <mika.laihanen@gmail.com>
2023-01-26 22:25:39 +0200

    Decluttering

--- server/src/database/db.mjs
+++ server/src/database/db.mjs
@@ -1,23 +1,8 @@
 import pg from 'pg';
 
-const pool = new pg.Pool({
+export const pool = new pg.Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_USER_PW,
   database: process.env.DB_NAME ? process.env.DB_NAME : 'blogthing'
 })
-
-async function testDbConnection(host, user, password) {
-  const client = new pg.Client({
-    host,
-    user,
-    password
-  });
-  const response = await client.connect()
-    .then(() => { return { status: true }})
-    .catch((err) =>  { return { status: false, message: err }});
-  client.end();
-  return response;
-}
-
-export { testDbConnection }
--- server/src/handlers/database.mjs
+++ server/src/handlers/database.mjs
@@ -1,26 +1,19 @@
-import { testDbConnection } from "../database/db.mjs"
+import pg from "pg";
 
 // Test connection
 export const testConnection = async (req, res) => {
   const creds = atob(req.headers.authorization)
-  const [user, pass] = creds.split(":");
-  const db = req.body.database;
-  let result = await testDbConnection(db, user, pass);
+  const [user, password] = creds.split(":");
+  const host = req.body.database;
+  const client = new pg.Client({ host, user, password });
+  const response = await client.connect()
+    .then(() => { return { status: true }})
+    .catch((err) =>  { return { status: false, message: err }});
+  client.end();
   
-  if (result.status) {
-    res.json({
-      status: "Success",
-      db,
-      user,
-      pass
-    })
+  if (response.status) {
+    res.json({ status: "Success" })
   } else {
-    res.json({
-      status: "Failure",
-      db,
-      user,
-      pass,
-      message: result.message
-    })
+    res.json({ status: "Failure", message: response.message })
   }
 };
